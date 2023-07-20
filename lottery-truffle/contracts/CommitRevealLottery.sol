554ee6cb05d48c21b002991cd4c50a34d648b655
Han-16 <haanbk16@gmail.com>
2023-07-21 01:03:24 +0900

    commitRevealLottery test 완료

--- lottery-truffle/contracts/CommitRevealLottery.sol
+++ lottery-truffle/contracts/CommitRevealLottery.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
+
 contract CommitRevealLottery {
     uint256 public commitCloses;
     uint256 public revealCloses;
@@ -9,19 +10,22 @@ contract CommitRevealLottery {
     address[] public players;
     address public winner;
     bytes32 seed;
-
-    mapping(address => bytes32) public commitments;
-    mapping(uint256 => address) public lotteryHistory;
+    mapping (address => bytes32) public commitments;
+    mapping (uint256 => address) public lotteryHistory;
 
     constructor() {
         commitCloses = block.number + DURATION;
         revealCloses = commitCloses + DURATION;
     }
 
-    function enter(bytes32 commitment) public payable {
-        require(msg.value >= .01 ether, "msg.value should be greater than or equal to 0.01 ETH");
-        require(block.number < commitCloses, "commit duration is over");
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
 
+    function enter(bytes32 commitment) public payable {
+        require(msg.value >= .01 ether, "msg.value should be greater than or equal to 0.01 ether");
+        require(block.number < commitCloses, "commit duration is over");
+        
         commitments[msg.sender] = commitment;
     }
 
@@ -29,13 +33,21 @@ contract CommitRevealLottery {
         return keccak256(abi.encodePacked(msg.sender, secret));
     }
 
+    function isAlreadyRevealed() public view returns (bool) {
+        for (uint256 i; i < players.length; i++) {
+            if (msg.sender == players[i]) return true;
+        }
+        return false;
+    }
+
     function reveal(uint256 secret) public {
         require(block.number >= commitCloses, "commit duration is not closed yet");
         require(block.number < revealCloses, "reveal duration is already closed");
-
+        require(!isAlreadyRevealed(), "You already revealed");
+        
         bytes32 commit = createCommitment(secret);
         require(commit == commitments[msg.sender], "commit not matches");
-
+        
         seed = keccak256(abi.encodePacked(seed, secret));
         players.push(msg.sender);
     }
@@ -51,8 +63,9 @@ contract CommitRevealLottery {
     }
 
     function withdrawPrize() public {
-        require(msg.sender == winner, "You are not the winner");
+        require(msg.sender == winner, "You're not the winner");
 
+        // initialize for next phase
         delete winner;
         for (uint256 i = 0; i < players.length; i++) {
             delete commitments[players[i]];
@@ -63,7 +76,7 @@ contract CommitRevealLottery {
         commitCloses = block.number + DURATION;
         revealCloses = commitCloses + DURATION;
 
-        (bool success, ) = payable(msg.sender).call{ value : address(this).balance }("");
-        require(success, "Failed to send ETH");
+        (bool success, ) = payable(msg.sender).call{ value: address(this).balance }("");
+        require(success, "Failed to send Ether to winner");
     }
 }
--- lottery-truffle/test/commitRevealLottery.test.js
+++ lottery-truffle/test/commitRevealLottery.test.js
@@ -40,5 +40,46 @@ contract("CommitRevealLottery", accounts => {
 
             await truffleAssert.reverts(commitRevealLottery.enter(commit, { from: accounts[1], value: enterAmount }), "msg.value should be greater than or equal to 0.01 ether");
         });
+
+        it("Enter 3 players and check values", async () => {
+            const enterAmount = web3.utils.toWei("0.01", "ether");
+
+            // player1 enter
+            const secret1 = 12345;
+            const commit1 = web3.utils.keccak256(web3.utils.encodePacked({ value: accounts[1], type: "address" }, { value: secret1, type: "uint256" }));
+            console.log(`commit1: ${commit1}`);
+
+            await commitRevealLottery.enter(commit1, { from: accounts[1], value: enterAmount });
+
+            // check values
+            assert.equal(await commitRevealLottery.getBalance(), enterAmount, "0.01 ETH not sent correctly by account1");
+            assert.equal(await commitRevealLottery.commitments(accounts[1]), commit1);
+
+            // player2 enter
+            const secret2 = 12346;
+            const commit2 = web3.utils.keccak256(web3.utils.encodePacked({ value: accounts[2], type: "address" }, { value: secret2, type: "uint256" }));
+            console.log(`commit2: ${commit2}`);
+
+            await commitRevealLottery.enter(commit2, { from: accounts[2], value: enterAmount });
+            assert.equal(await commitRevealLottery.getBalance(), web3.utils.toBN(enterAmount).mul(web3.utils.toBN(2)).toString());
+            assert.equal(await commitRevealLottery.commitments(accounts[2]), commit2);
+
+            // player3 enter
+            const secret3 = 12347;
+            const commit3 = web3.utils.keccak256(web3.utils.encodePacked({ value: accounts[3], type: "address" }, { value: secret3, type: "uint256" }));
+            console.log(`commit3: ${commit3}`);
+
+            await commitRevealLottery.enter(commit3, { from: accounts[3], value: enterAmount });
+            assert.equal(await commitRevealLottery.getBalance(), web3.utils.toBN(enterAmount).mul(web3.utils.toBN(3)).toString());
+            assert.equal(await commitRevealLottery.commitments(accounts[3]), commit3);
+
+            // player4 enter should revert
+            const secret4 = 12348;
+            const commit4 = web3.utils.keccak256(web3.utils.encodePacked({ value: accounts[4], type: "address" }, { value: secret4, type: "uint256" }));
+            console.log(`commit4: ${commit4}`);
+
+            await truffleAssert.reverts(commitRevealLottery.enter(commit4, { from: accounts[4], value: enterAmount }), "commit duration is over");
+            // await commitRevealLottery.enter(commit4, { from: accounts[4], value: enterAmt });
+        });
     });
 });
